be7557b84c2befa6c4569375ef418dd285f9154a
oschwery <oschwery@vols.utk.edu>
2016-05-10 02:18:53 -0400

    skeleton ready

--- BiSSEbox.R
+++ BiSSEbox.R
@@ -3,15 +3,17 @@ library(ape)
 library(geiger)
 library(diversitree)
 #Code to run BiSSEbox
-BiSSEbox <- function(tree, data) {
+BiSSEbox <- function(tree, data, Nsteps=5000) {  # MCMCgens, samp.freq, taxofile
   # generate output objects
   traitMLfits <- list()
   MCMCs <- list()
+  AICs <- list()
+  traits <- colnames(data)
 
   for (i in length(traits) {
     ##chose desired setup (no missing sampling, richness  file, sampling frequency)
-    current.trait <- traits[, i]
-    names(current.trait) <- row.names(traits)
+    current.trait <- data[, i]
+    names(current.trait) <- row.names(data)
     lik <- make.bisse(tree, current.trait)
     #lik <- make.bisse(tree, SLA.v, unresolved=richnessSLA)
     #sampling.f_SLA<-c(0.051095,0.046405)
@@ -44,7 +46,7 @@ BiSSEbox <- function(tree, data) {
     fit.mq<- find.mle(lik.mq, p[argnames(lik.mq)])
     fit.lmq<- find.mle(lik.lmq, p[argnames(lik.lmq)])
     #save the fits
-    MLfits <- list(unconstrained=fit, equal.lambda=fit.l, equal.mu=fit.m, equal.q=fit.q, equal.lambda.mu=fit.lm, equal.lambda.q=fit.lq, equal.mu.q=fit.mq, all.equal=fit.lmq)
+    traitMLfits[[i]] <- list(unconstrained=fit, equal.lambda=fit.l, equal.mu=fit.m, equal.q=fit.q, equal.lambda.mu=fit.lm, equal.lambda.q=fit.lq, equal.mu.q=fit.mq, all.equal=fit.lmq)
     #round(rbind(full=coef(fit), equal.l=coef(fit.l, TRUE)), 3)
     #round(rbind(full=coef(fit), equal.m=coef(fit.m, TRUE)), 3)
     #round(rbind(full=coef(fit), equal.q=coef(fit.q, TRUE)), 3)
@@ -54,28 +56,36 @@ BiSSEbox <- function(tree, data) {
     #round(rbind(full=coef(fit), equal.lmq=coef(fit.lmq, TRUE)), 3)
 
     ##ANOVA to test if constrained and unconstrained model perform different
-    anova(fit, equal.l=fit.l, equal.m=fit.m, equal.q=fit.q, equal.lm=fit.lm, equal.lq=fit.lq, equal.mq=fit.mq, equal.lmq=fit.lmq )
+    #anova(fit, equal.l=fit.l, equal.m=fit.m, equal.q=fit.q, equal.lm=fit.lm, equal.lq=fit.lq, equal.mq=fit.mq, equal.lmq=fit.lmq )
+    AllLiks <- c(lik, lik.l, lik.m, lik.q, lik.lm, lik.lq, lik.mq, lik.lmq)
+    AllFits <- c(fit, fit.l, fit.m, fit.q, fit.lm, fit.lq, fit.mq, fit.lmq)
+    Modelnames <- c("unconstrained", "equal.lambda", "equal.mu", "equal.q", "equal.lambda.mu", "equal.lambda.q", "equal.mu.q, all.equal")
+    # add AIC's hethere...
+    currentAICs <- c()
+    for (j in 1:length(Modelnames)) {
+      currentAICs <- c(currentAICs, AIC(AllFits[j]))
+    }
+    names(currentAICs) <- Modelnames
 
-# add AIC's hethere...
-AIC(fit)
-logLik(fit)
-coef(fit)
+    currentlogLiks <- c()
+    for (j in 1:length(Modelnames)) {
+      currentlogLiks <- c(currentlogLiks, logLik(AllFits[j]))
+    }
+    names(currentlogLiks) <- Modelnames
 
+    currentcoefs <- list()
+    for (j in 1:length(Modelnames)) {
+      currentcoefs[[j] <- coef(AllFits[j]))
+    }
+    names(currentcoefs) <- Modelnames
 
-    AIC.uncon <- (-2*(ln(likelihood))+(2*6))
-    AIC.l <-
-    AIC.m <-
-    AIC.q <-
-    AIC.lm <-
-    AIC.lq <-
-    AIC.mq <-
-    AIC.lmq <-
+#    suffix <- c("l", "m", "ml")
+#    for (i in sequence(lenght))
 
-    suffix <- c("l", "m", "ml")
-    for (i in sequence(lenght))
-
-    selected.model.lik <- ...
-    selected.model.fit <- ...
+    selected.model.lik <- AllLiks[which(currentAICs == min(currentAICs))]
+    selected.model.fit <- AllFits[which(currentAICs == min(currentAICs))]
+    selected.model.name <- Modelnames[which(currentAICs == min(currentAICs))]
+    print(selected.model.name)
     # run MCMC of that one
     prior <- make.prior.exponential(2*(log(length(tree$tip.label))/(max(branching.times(tree)))))
 
@@ -84,24 +94,24 @@ coef(fit)
     w=diff(sapply(mcmc.bisse[2:5],quantile,c(0.05,0.95)))
 
     #For real...
-    mcmc.bisse2<-mcmc(selected.model.lik,selected.model.fit$par,nsteps=5000,w=w,prior=prior)
+    mcmc.bisse2<-mcmc(selected.model.lik,selected.model.fit$par,nsteps=Nsteps,w=w,prior=prior)
 
     #Now we can examine the 95% credible intervals of the posterior samples for each parameter.  If the intervals do not overlap, then we have posterior Bayesian support for a difference in rates.
-    sapply(mcmc.bisse2[,2:6],quantile,c(0.025,0.975))
+    #sapply(mcmc.bisse2[,2:6],quantile,c(0.025,0.975))
 
 
-    save outputs (all the fit, round table of fits, anova, aic, chosen model, mcmc.bisse2)
+    save outputs (all the fit, round table of fits (no, let auxiliary function do that), aic, chosen model  [as name of -> ], mcmc.bisse2)
 
-    traitMLfits <- c(traitMLfits, MLfits)
+#    traitMLfits <- c(traitMLfits, MLfits)
 
-    MCMCs <- c(MCMCs, mcmc.bisse2)
+    MCMCs[[i]] <- mcmc.bisse2
+    name(MCMCs[[i]]) <- selected.model.name
 
-
-    clear all the objects?
-    MLfits <- list()
+#    clear all the objects?
+#    MLfits <- list()
   }
 
-  return(list(fits=traitMLfits, tablesofrates=roundtableofallthefits, ANOVA=anova, AICs=AICs, ChosenModels=ChosenModels, MCMCs=MCMCs))
+  return(list(fits=traitMLfits, AICs=AICs, ChosenModels=ChosenModels, MCMCs=MCMCs))
 
   Save output to allow seeing all and plotting all and having a summary of the results
 }
